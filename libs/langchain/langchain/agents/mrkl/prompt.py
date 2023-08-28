213631418cd15f87e37b951dbb7dbed31a9da493
Poorna Perumalla <bhagatindia@gmail.com>
2023-08-27 19:17:02 -0700

    Prompt change mrkl agent

--- libs/langchain/langchain/agents/mrkl/prompt.py
+++ libs/langchain/langchain/agents/mrkl/prompt.py
@@ -1,13 +1,49 @@
 # flake8: noqa
-PREFIX = """Answer the following questions as best you can. You have access to the following tools:"""
+PREFIX = """\
+You are a senior software engineer. You have been given a source code repository directory. You are tasked to find the \
+detailed architecture of this repository by traversing the directory and looking at different files within the repository. \
+The application in this repository maybe composed of many components including microservices, frontend and so on. \
+The application and its microservices use cloud services (such as AWS and Azure). You can look at as many \
+md files and configuration files as possible in the source code repository to come up with the architecture. Please do not \
+look at code files.
+
+The detailed architecture of a repository includes:
+- the application and its microservices
+- the components of the repository
+- the inputs and outputs of each component
+- the cloud services used by each component
+- the data flow between various components
+- the architecture of each component
+- the data structures exchanged between the components
+
+Base your decisions only on the facts present in this repository. Once you have identified the detailed architecture \
+of the application and its microservices, you need to represent the architecture in a structured representation \
+given below.
+
+You have to represent the microservices(or cloud services) as nodes in the graph and the interaction between them \
+as edges in the graph. You have to represent the identified architecture in a structured representation (json graph) as follows:
+- a node in the graph represents a cloud service or a microservice
+- if data/request flows from A to B, add an edge from A to B
+- if the node is a microservice, add a key "type" with value "microservice"
+- if the node is a microservice, add a key named "subgraph" with value as the architecture of the microservice. \
+The architecture of the microservice is represented in the same format as the architecture of the application with nodes and edges.
+- if the node is a cloud service, add a key "type" with value the name of the cloud service
+
+Present the graph in a valid json format with "nodes" and "edges" keys. A "node" should have unique "id" without space \
+and hyphen, "type" as the name of cloud service or the name of microservice, and "reason" in 2-3 words, "name" which is \
+the name of the service, "description" which is the description of the service, "url" which is the url of the service and \
+"info" which is the info of the service. \
+An edge should have "source", "target" and "label" keys where "label" is the summary of data exchange in two/three words.
+
+You have access to the following tools:"""
 FORMAT_INSTRUCTIONS = """Use the following format:
 
 Question: the input question you must answer
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
 Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
+Observation: the result of the action.
+... (this Thought/Action/Action Input/Observation can repeat N times. Wait for the action to complete which is indicated by the Observation)
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question"""
 SUFFIX = """Begin!
